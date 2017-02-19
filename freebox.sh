71ac88e15fbd82d20ccb1735dfdc140c6f1fd4e3
WEBER Logan <weberlogan@gmail.com>
2017-02-19 20:17:27 +0100

    Refactoring

--- freebox.sh
+++ freebox.sh
@@ -6,6 +6,23 @@ function ts {
 
 #-----------------------------------------------------------------------------------------------------------------------
 
+# Same as call_freebox_api function but with a new optional param : HTTP verb (useful for DELETE queries for example)
+function fb_call_freebox_api {
+    local api_url="$1"
+    local verb="${2-}"
+    local data="${3-}"
+    local options=("")
+    local url="$FREEBOX_URL"$( echo "/$_API_BASE_URL/v$_API_VERSION/$api_url" | sed 's@//@/@g')
+    [[ -n "$_SESSION_TOKEN" ]] && options+=(-H "X-Fbx-App-Auth: $_SESSION_TOKEN")
+    [[ -n "$verb" ]] && options+=(-X "$verb")
+    [[ -n "$data" ]] && options+=(-d "$data")
+    answer=$(curl -s "$url" "${options[@]}")
+    _check_success "$answer" || return 1
+    echo "$answer"
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
 function fb_authorize {
  if [ ! -f /config/freebox_auth.conf ]; then
    echo "$(ts) [DEBUG] /config/freebox_auth.conf missing"
@@ -24,7 +41,7 @@ function fb_authorize {
 #-----------------------------------------------------------------------------------------------------------------------
 
 function fb_logout {
- answer_logout=$(call_freebox_api '/login/logout/' "{}")
+ local answer_logout=$(call_freebox_api '/login/logout/' "{}")
  if ! _check_success "$answer_logout"; then
   echo "$(ts) [ERROR] Failed to close session"
   return 1
@@ -41,6 +58,55 @@ function fb_iterate {
  echo "$(ts) [INFO] Wait for $LOOP_DELAY seconds..."
  sleep $LOOP_DELAY
  continue
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+function fb_manage_completed_dls {
+ # Filter only done or seeding downloads
+ local files=$(echo "$1" | jq -r '.result[]? | select(.status == "seeding") | .name')
+ if [ -z "$files" ] || [ ${#files[@]} -eq 0 ]; then
+  echo "$(ts) [INFO] No completed downloads detected"
+ else
+  local has_new_files=false
+  for file in "$files"
+   do
+    echo "$(ts) [INFO] New completed download detected : ${file}"
+    if ! grep -Fxq "$file" /config/fb-exclude-list.txt; then
+     echo "$(ts) [DEBUG] $file not found in freebox exclude list"
+     echo "$(ts) [INFO] Copy /freebox/${file} to /completed/${file}"
+     if cp -rf /freebox/"${file}" /completed/"${file}"; then
+      echo "$(ts) [DEBUG] Add ${file} to freebox exclude list"
+      echo "$file" >> /config/fb-exclude-list.txt
+     fi
+    else
+     echo "$(ts) [WARN] $file found in freebox exclude list, ignoring"
+    fi
+   done
+  if $has_new_files; then
+   echo "$(ts) [INFO] Starting filebot script"
+   /files/runas.sh $USER_ID $GROUP_ID $UMASK /files/filebot.sh
+  fi
+ fi
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+function fb_manage_seeded_dls {
+ # Filter only done or seeding downloads
+ local ids=$(echo "$1" | jq -r '.result[]? | select(.status == "done") | .id')
+ if [ -z "$ids" ] || [ ${#ids[@]} -eq 0 ]; then
+  echo "$(ts) [INFO] No seeded downloads detected"
+ else
+  for id in "$ids"
+   do
+    echo "$(ts) [INFO] New seeded download detected : ${id}, try to erase it"
+    local answer_delete=$(fb_call_freebox_api "/downloads/${id}/erase" "DELETE")
+    if ! _check_success "$answer_delete"; then
+     echo "$(ts) [ERROR] Failed to erase download ${id}"
+    fi
+   done
+ fi
 }
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -75,30 +141,9 @@ function run {
     fb_iterate true
    fi
    echo "$(ts) [DEBUG] Downloads response : $answer_dls"
-   # Filter only done or seeding downloads
-   files=$(echo "$answer_dls" | jq -r '.result[]? | select(.status == "done" or .status == "seeding") | .name')
-   if [ -z "$files" ] || [ ${#files[@]} -eq 0 ]; then
-    echo "$(ts) [INFO] No completed downloads detected"
-   else
-    local has_new_files=false
-    for file in "$files"
-     do
-      echo "$(ts) [INFO] New completed download detected : ${file}"
-      if ! grep -Fxq "$file" /config/fb-exclude-list.txt; then
-       echo "$(ts) [DEBUG] $file not found in freebox exclude list"
-       echo "$(ts) [INFO] Copy /freebox/${file} to /completed/${file}"
-       if cp -rf /freebox/"${file}" /completed/"${file}"; then
-         echo "$(ts) [DEBUG] Add ${file} to freebox exclude list"
-         echo "$file" >> /config/fb-exclude-list.txt
-       fi
-      else
-        echo "$(ts) [WARN] $file found in freebox exclude list, ignoring"
-      fi
-     done
-     if $has_new_files; then
-      echo "$(ts) [INFO] Starting filebot script"
-      /files/runas.sh $USER_ID $GROUP_ID $UMASK /files/filebot.sh
-     fi
+   fb_manage_completed_dls "$answer_dls"
+   if [ ! -z "$ERASE_SEEDED_DLS" ] && $ERASE_SEEDED_DLS; then
+    fb_manage_seeded_dls "$answer_dls"
    fi
    fb_iterate true
   done
